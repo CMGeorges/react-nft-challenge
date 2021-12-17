7e6fa81cd715b14d438817a5c4215e4f1959c7aa
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2021-12-16 19:06:15 -0500

    Add PunkList Component :fire:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
-import CollectionCard from './components/CollectionCard';
-import Header from './components/Header';
+import  axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import CollectionCard from "./components/CollectionCard";
+import Header from "./components/Header";
+import PunkList from "./components/PunkList";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    const getMyNFTs = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x0fe04Ac44ef8bD439Af822dDC798a7262dc53d0a&order_direction=asc"
+      );
+
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets)
+    };
+
+    return getMyNFTs();
+  }, []);
+
   return (
     <div className="app">
-
       <Header />
-      <CollectionCard id={0} name={'Bandana Punk'} traits={[{'value':7}]} image='https://lh3.googleusercontent.com/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q=w600' />
+      <PunkList  punkListData={punkListData}/>
     </div>
   );
 }
